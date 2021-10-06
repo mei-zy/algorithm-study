45cd8dae5d66a088e76bacd83c9d53679a8dfbbc
urther <alswlkkU@ajou.ac.kr>
2021-10-06 12:51:29 +0900

    modified algo

--- 과제/1주차(문자열,해싱, 배열)/문자열구분.js
+++ 과제/1주차(문자열,해싱, 배열)/문자열구분.js
@@ -1,7 +1,21 @@
 function solution(s){
-  // 정규식 이용.. ? 
+  s.sort((x,y)=>x.length-y.length);
+  // 1. 글자길이 오름차순 정렬
+  let size=s[0].length;
   
+  let cnt=0;
+  for(let i=0;i<size;i++){
+    cnt++;
+    if(s[0][i]==s[1][i] && s[0][i]==s[2][i]){
+      continue;
+    }
+    else
+      break;
+
+  }
+  return cnt;
+
 }
 
-let array=["longlong","longtong","longbig"];
+let array=["seeasue","sesseysu","semeas"];
 console.log(solution(array));
