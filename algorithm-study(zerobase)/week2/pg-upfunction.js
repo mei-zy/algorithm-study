da525710f2092d772c046d194f80c92daf713848
urther <alswlkkU@ajou.ac.kr>
2021-10-17 16:07:17 +0900

    programmers - 기능개발(solved)

--- algorithm-study(zerobase)/week2/pg-upfunction.js
+++ algorithm-study(zerobase)/week2/pg-upfunction.js
@@ -1,21 +1,20 @@
 function solution(progresses, speeds) {
-  var answer = [0];
-  // 1. 몇일 걸리는지 체크
-  let days=[];
-  for(let i=0;i<progresses.length;i++){
-    days[i]=(100-progresses[i])/speeds[i];
-    if(!Number.isInteger(days[i])) days[i]=parseInt(days[i])+1;
-    // float 타입이면 integer형으로 변경후 +1 일
-  }
-  // 2. 스택에 넣으면서 체크
+  var answer = [];
+  let len=progresses.length;
+    // len : 총 해야하는 progress 의 갯수
   let stack=[];
-  for(let i=0, j=0;i<days.length;i++){
-    if(days[j]>=days[i])
-      answer[j]+=1;
-    else{
-      answer[++j]+=1;
-      console.log(j)
+  for(let i=len-1 ;i>=0;i--){
+    stack.push(Math.ceil((100-progresses[i])/speeds[i]));
+  }
+  while(stack.length){
+    let top=stack.pop();
+
+    let cnt=1;
+    while(stack[stack.length-1]<=top){
+      stack.pop();
+      cnt++
     }
+    answer.push(cnt);
   }
   return answer;
 }
