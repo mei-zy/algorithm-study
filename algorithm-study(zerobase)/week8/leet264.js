94b5c2dfa7b0b81ef7f34ba96a173206e07df02f
urther <alswlkkU@ajou.ac.kr>
2021-11-25 16:14:39 +0900

    leetcode - 264 : ugly number2 (solved!)

---
+++ algorithm-study(zerobase)/week8/leet264.js
@@ -0,0 +1,19 @@
+function uglyNumber(n) {
+  let p2 = 1;
+  let p3 = 1;
+  let p5 = 1;
+  let arr = new Array(n + 1);
+
+  let min;
+  arr[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    min = Math.min(arr[p2] * 2, arr[p3] * 3, arr[p5] * 5);
+    // console.log("p2 : ", p2, " p3 : ", p3, " p5", p5);
+    if (min === arr[p2] * 2) p2++;
+    if (min === arr[p3] * 3) p3++;
+    if (min === arr[p5] * 5) p5++;
+    arr[i] = min;
+  }
+  return arr[n];
+}
+console.log(uglyNumber(11));
