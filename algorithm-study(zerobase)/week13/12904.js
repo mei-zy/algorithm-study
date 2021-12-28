9be7ec479d0886377224f383321c602e8f48a0c3
urther <alswlkku@ajou.ac.kr>
2021-12-28 16:48:41 +0900

    [boj] 12904 A와B (solved!)
    - in sameString func , use for nor join . need to modify

--- algorithm-study(zerobase)/week13/12904.js
+++ algorithm-study(zerobase)/week13/12904.js
@@ -2,25 +2,27 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const S = input[0];
-const T = input[1];
+const S = input[0].split("");
+const T = input[1].split("");
 // console.log(S, T);
 
-let queue = [];
-let setH = new Set();
-queue[queue.length] = S;
-const BFS = () => {
-  while (queue.length) {
-    let tmpString = queue.shift();
-    if (tmpString === T) return 1;
-    if (tmpString.length > T.length) return 0;
-
-    let addToLastA = tmpString + "A";
-    let reverseStrAddToB = tmpString.split("").reverse().join("") + "B";
-
-    if (!setH.has(addToLastA)) queue.push(addToLastA);
-    if (!setH.has(reverseStrAddToB)) queue.push(reverseStrAddToB);
-  }
+let answer = 0;
+const sameString = (str1, str2) => {
+  return str1.join("") === str2.join("") ? true : false;
 };
+while (true) {
+  if (S.length === T.length) {
+    if (sameString(S, T)) {
+      answer = 1;
+      break;
+    }
+  }
+  if (T[T.length - 1] === "A") {
+    T.pop();
+  } else {
+    T.pop();
+    T.reverse();
+  }
+}
 
-console.log(BFS());
+console.log(answer);
