4b498942899491749f087c7afc08ad806120d8a3
urther <alswlkku@ajou.ac.kr>
2022-02-08 14:55:17 +0900

    [boj] 3190 뱀 (solved!)

--- algorithm-study(zerobase)/2022_02/3190.js
+++ algorithm-study(zerobase)/2022_02/3190.js
@@ -8,13 +8,13 @@ const inputs = require("fs")
 const n = Number(inputs.shift());
 const k = Number(inputs.shift());
 
-let board = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+let board = Array.from(Array(n + 2), () => Array(n + 2).fill(0));
 // board 벽 처리
 for (let i = 0; i <= n; i++) {
   board[0][i] = 1;
   board[i][0] = 1;
-  board[n][i] = 1;
-  board[i][n] = 1;
+  board[n + 1][i] = 1;
+  board[i][n + 1] = 1;
 }
 
 // 사과가 있는 자리 : 2
@@ -40,37 +40,51 @@ let second = 0;
 
 snake.push([1, 1]);
 
+let flag = 0;
 while (snake.length) {
-  let [snakehead_x, snakehead_y] = snake[0];
-
-  // 벽이 있는 경우
-  if (board[snakehead_x][snakehead_y] === 1) {
-    break;
-  }
-  // 사과가 있는 경우
-  else if (board[snakehead_x][snakehead_y] === 2) {
-    snake.unshift([
-      snakehead_x + dx[current_dir],
-      snakehead_y + dy[current_dir],
-    ]);
-  } else {
-    // 아무것도 없는 경우
-    snake.unshift([
-      snakehead_x + dx[current_dir],
-      snakehead_y + dy[current_dir],
-    ]);
-
-    snake.pop();
-  }
-  // direction 변경
-  // second +1 초후 변경해줌
-  if (dir.has(second)) {
-    let direction = dir.get(second);
-
-    if (direction === "L") current_dir = (current_dir + 3) % 4;
-    else current_dir = (current_dir + 1) % 4;
-  }
+  // 뱀 머리
+  let [snake_x, snake_y] = snake[0];
   second++;
-  // direction 변경해주기
+  // 현재 위치를 변경해야하는 경우
+  if (dir.has(second - 1)) {
+    let change_dir = dir.get(second - 1);
+
+    if (change_dir === "D") {
+      current_dir = (current_dir + 1) % 4;
+    } else {
+      current_dir = (current_dir + 3) % 4;
+    }
+  }
+
+  // 현재 board 위의 snake 머리 위치
+  let current_snake =
+    board[snake_x + dx[current_dir]][snake_y + dy[current_dir]];
+
+  /* 조건문
+  1. 벽을 만나는 경우
+  2. 사과가 있는 경우 - 뱀의 길이만 늘어난다 
+  3. 사과가 없는 경우 */
+  if (current_snake === 1) break;
+  else {
+    let move_head_x = snake_x + dx[current_dir];
+    let move_head_y = snake_y + dy[current_dir];
+
+    snake.unshift([move_head_x, move_head_y]);
+    if (current_snake === 2) {
+      // 사과를 없애줌
+      board[move_head_x][move_head_y] = 0;
+    }
+    if (current_snake !== 2) {
+      for (let i = snake.length - 1; i >= 1; i--) {
+        let [tmpx, tmpy] = snake[i];
+        if (tmpx === move_head_x && tmpy === move_head_y) {
+          flag = 1;
+          break;
+        }
+      }
+      snake.pop();
+    }
+  }
+  if (flag) break;
 }
 console.log(second);
