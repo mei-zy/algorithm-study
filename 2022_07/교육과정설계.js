3144d7cd7bbe98d823556d60fc73baf70818adf0
urther <alswlkku@ajou.ac.kr>
2022-06-14 20:19:19 +0900

    교육과정 설계

---
+++ 2022_07/교육과정설계.js
@@ -0,0 +1,25 @@
+const solution = (list, plan) => {
+  let answer = "YES";
+  let queue = list.split("");
+
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      // 이수과목이면
+      if (x !== queue.shift()) {
+        // 이수과목에 포함되어있지만 순서가 안맞다는 뜻
+        return "NO";
+      } else {
+        // 이수과목에 포함되어있고 순서도 맞으면 큐에서 해당과목 제거
+        queue.shift();
+      }
+    }
+  }
+  if (queue.length > 0) {
+    return "NO";
+  }
+  return answer;
+};
+
+const list = "CBA";
+const plan = "CBDAGE";
+console.log(solution(list, plan));
